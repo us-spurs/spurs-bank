955c805b174d35b97d095e9976df0e678eee2532
dimka <dimka8445484@gmail.com>
2023-05-20 11:03:55 +0200

    changed: header

--- backend/src/app/app.service.ts
+++ backend/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common'
 @Injectable()
 export class AppService {
     getHello(): string {
-        return 'Hello World!dfrtffdgfdgh'
+        return 'Hello World!dfrtf'
     }
 }
