8f0769755ea6382f7d69993e850913ffa24ee3e2
dimka <dimka8445484@gmail.com>
2023-05-26 09:13:09 +0200

    added: new title

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
                 </h1>
 
                 <p className={styles.description}>
-                    Get started by editing <code className={styles.code}>pages/index.tsx</code>
+                    Get started by retererterterediting <code className={styles.code}>pages/index.tsx</code>
                 </p>
 
                 <div className={styles.grid}>
